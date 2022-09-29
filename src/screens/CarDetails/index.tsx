d7cb46952e77ac5c64b6be5530848c4b60bcd2a9
girardi1994 <30416996+girardi1994@users.noreply.github.com>
2022-09-29 15:58:09 -0300

    Componentes

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,21 +1,32 @@
 import React from "react";
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
+import { Accessory } from "../../components/Accessory";
+import { Button } from "../../components/Button";
 
-import { 
+import SpeedSvg from '../../assets/speed.svg'
+import AccelerationSvg from '../../assets/acceleration.svg'
+import ForceSvg from '../../assets/force.svg'
+import GasolineSvg from '../../assets/gasoline.svg'
+import PeopleSvg from '../../assets/people.svg'
+import ExchangeSvg from '../../assets/exchange.svg'
+
+import {
   Container,
-   Header, 
-   CarImages,
-   Content,
-   Details,
-   Descripton,
-   Brand,
-   Name,
-   Rent,
-   Period,
-   Price,
-   About,
-  } from "./styles";
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Descripton,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories,
+  Footer,
+} from "./styles";
 
 export function CarDetails() {
   return (
@@ -43,10 +54,20 @@ export function CarDetails() {
             <Price>R$ 580</Price>
           </Rent>
         </Details>
-        <About>
-          Texto aqui 
-        </About>
+        <Accessories>
+          <Accessory name='380Km/h' icon={SpeedSvg}/>
+          <Accessory name='3.2' icon={AccelerationSvg}/>
+          <Accessory name='800 HP' icon={ForceSvg}/>
+          <Accessory name='Gasolina' icon={GasolineSvg}/>
+          <Accessory name='Auto' icon={ExchangeSvg}/>
+          <Accessory name='2 Pessoas' icon={PeopleSvg}/>
+        </Accessories>
+        <About>Texto aqui</About>
       </Content>
+
+      <Footer>
+        <Button title="Confirmar"/>
+      </Footer>
     </Container>
   );
 }
